e82a08c31ab0c36fc7088467e41a846b07fa3d15
Fuhito Suguri <bitwalk@gmail.com>
2020-08-19 08:35:10 +0900

    add label/entry/button widgets

--- dllexplorer.py
+++ dllexplorer.py
@@ -1,4 +1,5 @@
 import gi
+import os
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
@@ -8,56 +9,82 @@ from module import utils
 
 class DLLExplorer(Gtk.Window):
     dir_target = '/home/bitwalk/test/usr/local/bin'
+    # CSS
+    provider = Gtk.CssProvider()
+    provider.load_from_path('./dllexplorer.css')
 
     def __init__(self):
         Gtk.Window.__init__(self, title='DLL Explorer')
-        self.set_default_size(600, 0)
+        self.set_default_size(800, 0)
 
         grid = Gtk.Grid(column_spacing=5)
         self.add(grid)
 
-        lab0 = Gtk.Label(label='app root')
-        lab0.set_hexpand(False)
-        lab0.set_halign(Gtk.Align.END)
+        lab00 = Gtk.Label(label='app root (source)')
+        lab00.set_hexpand(False)
+        lab00.set_halign(Gtk.Align.START)
+        context = lab00.get_style_context()
+        context.add_provider(self.provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
 
-        ent0 = Gtk.Entry()
-        ent0.set_hexpand(True)
-        ent0.set_editable(False)
-        ent0.set_can_focus(False)
+        self.ent01 = Gtk.Entry()
+        self.ent01.set_hexpand(True)
 
-        but0 = Gtk.Button()
-        but0.add(utils.img().get_image('folder'))
-        but0.connect('clicked', self.on_folder_clicked, ent0, self.dir_target)
+        but04 = Gtk.Button()
+        but04.add(utils.img().get_image('folder'))
+        but04.connect('clicked', self.on_get_app_root_dir)
 
-        lab1 = Gtk.Label(label='pkg root')
-        lab1.set_hexpand(False)
-        lab1.set_halign(Gtk.Align.END)
+        lab10 = Gtk.Label(label='pkg root (destination)')
+        lab10.set_hexpand(False)
+        lab10.set_halign(Gtk.Align.START)
+        context = lab10.get_style_context()
+        context.add_provider(self.provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
 
-        ent1 = Gtk.Entry()
-        ent1.set_hexpand(True)
-        ent1.set_editable(False)
-        ent1.set_can_focus(False)
+        self.ent11 = Gtk.Entry()
+        self.ent11.set_hexpand(True)
 
-        but1 = Gtk.Button()
-        but1.add(utils.img().get_image('folder'))
+        lab12 = Gtk.Label(label='/')
+        lab12.set_hexpand(False)
+
+        self.ent13 = Gtk.Entry()
+        self.ent13.set_hexpand(False)
+        self.ent13.set_width_chars(20)
+
+        but14 = Gtk.Button()
+        but14.add(utils.img().get_image('folder'))
+        but14.connect('clicked', self.on_get_pkg_root_dir)
 
         but_start = Gtk.Button(label='START')
-        but_start.connect('clicked', self.on_start, self.dir_target)
+        but_start.connect('clicked', self.on_start)
 
-        grid.attach(lab0, 0, 0, 1, 1)
-        grid.attach(ent0, 1, 0, 1, 1)
-        grid.attach(but0, 2, 0, 1, 1)
-        grid.attach(lab1, 0, 1, 1, 1)
-        grid.attach(ent1, 1, 1, 1, 1)
-        grid.attach(but1, 2, 1, 1, 1)
-        grid.attach(but_start, 0, 2, 3, 1)
+        grid.attach(lab00, 0, 0, 1, 1)
+        grid.attach(self.ent01, 1, 0, 3, 1)
+        grid.attach(but04, 4, 0, 1, 1)
+        grid.attach(lab10, 0, 1, 1, 1)
+        grid.attach(self.ent11, 1, 1, 1, 1)
+        grid.attach(lab12, 2, 1, 1, 1)
+        grid.attach(self.ent13, 3, 1, 1, 1)
+        grid.attach(but14, 4, 1, 1, 1)
+        grid.attach(but_start, 0, 2, 5, 1)
 
-    def on_start(self, button, dir):
+    def on_start(self, button):
         print('START')
-        obj = utils.RunTime(dir)
+        obj = utils.RunTime(self.dir_target)
         obj.start()
 
-    def on_folder_clicked(self, widget, entry, dir):
+    def on_get_app_root_dir(self, widget):
+        dir = self.on_get_dir()
+        if dir is not None:
+            self.ent01.set_text(dir)
+            self.ent13.set_text(os.path.basename(dir))
+
+    def on_get_pkg_root_dir(self, widget):
+        dir = self.on_get_dir()
+        if dir is not None:
+            self.ent11.set_text(dir)
+
+    def on_get_dir(self):
+        dir = None
+
         dialog = Gtk.FileChooserDialog(title="Select folder",
                                        parent=self,
                                        action=Gtk.FileChooserAction.SELECT_FOLDER)
@@ -68,14 +95,10 @@ class DLLExplorer(Gtk.Window):
 
         response = dialog.run()
         if response == Gtk.ResponseType.OK:
-            print('Select button is selected')
-            print('Selected ' + dialog.get_filename())
-            entry.set_text(dialog.get_filename())
             dir = dialog.get_filename()
-        elif response == Gtk.ResponseType.CANCEL:
-            print('Cancel button is selected')
 
         dialog.destroy()
+        return dir
 
 
 # -----------------------------------------------------------------------------
